02066459a59284a28219d5a82264f6b69f873744
luis444coelho <a106843@alunos.uminho.pt>
2026-03-06 15:02:45 +0000

    add: página principal

--- TPC5/cinema/routes/index.js
+++ TPC5/cinema/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  var d = new Date().toISOString().substring(0, 16)
+  axios.get("http://localhost:3000/filmes")
+        .then(resp => { 
+            var filmes = resp.data
+             res.render('index', { list: filmes, date: d });
+        })
+})
+
+router.get('/filmes/:id', function(req, res, next) {
+  var d = new Date().toISOString().substring(0, 16)
+    axios.get('http://localhost:3000/filmes/' + req.params.id)
+                        .then(resp => { 
+                            var filmes = resp.data
+                            res.render('filmes', { filme: filmes, date: d });
+                        })
+})
+
 
 module.exports = router;
